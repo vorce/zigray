1c8e7d6d7a55da96f0cd077194721e250f708626
Joel C <joel@vorce.se>
2021-11-29 16:39:49 +0100

    Sending Rays Into the Scene

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,23 @@
 const std = @import("std");
 const stderr = std.debug;
 const stdout = std.io.getStdOut().writer();
-const vector = @import("vector.zig");
-const Vec3 = vector.Vec3;
+const Vec3 = @import("vector.zig").Vec3;
+const Ray = @import("ray.zig").Ray;
 
 pub fn main() anyerror!void {
-    const image_width: u32 = 256;
-    const image_height: u32 = 256;
+    const aspect_ratio: f32 = 16.0 / 9.0;
+    const image_width: u32 = 400;
+    const image_height: u32 = @floatToInt(u32, @intToFloat(f32, image_width) / aspect_ratio);
+
+    // Camera
+    const viewport_height: f32 = 2.0;
+    const viewport_width: f32 = aspect_ratio * viewport_height;
+    const focal_length: f32 = 1.0;
+
+    const origin: Vec3 = Vec3.init(0.0, 0.0, 0.0);
+    const horizontal: Vec3 = Vec3.init(viewport_width, 0.0, 0.0);
+    const vertical: Vec3 = Vec3.init(0.0, viewport_height, 0.0);
+    const lower_left_corner: Vec3 = origin.sub(horizontal.divideBy(2.0)).sub(vertical.divideBy(2.0)).sub(Vec3.init(0.0, 0.0, focal_length));
 
     try stdout.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
@@ -16,7 +27,11 @@ pub fn main() anyerror!void {
 
         var x: i32 = 0;
         while (x < image_width) : (x += 1) {
-            var pixel_color: Vec3 = Vec3.init(@intToFloat(f32, x) / @intToFloat(f32, (image_width - 1)), @intToFloat(f32, y) / @intToFloat(f32, (image_height - 1)), 0.25);
+            var u = @intToFloat(f32, x) / @intToFloat(f32, image_width - 1);
+            var v = @intToFloat(f32, y) / @intToFloat(f32, image_height - 1);
+            var ray = Ray.init(origin, lower_left_corner.add(horizontal.multiplyBy(u)).add(vertical.multiplyBy(v)).sub(origin));
+            var pixel_color = ray.color();
+
             try Vec3.writeColor(pixel_color);
         }
     }
--- src/ray.zig
+++ src/ray.zig
@@ -16,6 +16,15 @@ pub const Ray = struct {
     pub fn at(self: Ray, distance: f32) Vec3 {
         return self.origin.add(self.direction.multiplyBy(distance));
     }
+
+    pub fn color(ray: Ray) Vec3 {
+        const unit_direction: Vec3 = Vec3.unit(ray.direction);
+        const distance: f32 = 0.5 * (unit_direction.y + 1.0);
+        const fromColor: Vec3 = Vec3.init(1.0, 1.0, 1.0);
+        const toColor: Vec3 = Vec3.init(0.5, 0.7, 1.0);
+
+        return fromColor.multiplyBy(1.0 - distance).add(toColor.multiplyBy(distance));
+    }
 };
 
 test "at" {
