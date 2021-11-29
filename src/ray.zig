a27ac45bfbec239a59a9b0528ccc80e22b54e5ea
Joel C <joel@vorce.se>
2021-11-29 17:02:43 +0100

    A sphere colored according to its normal vectors

--- src/ray.zig
+++ src/ray.zig
@@ -17,26 +17,33 @@ pub const Ray = struct {
         return self.origin.add(self.direction.multiplyBy(distance));
     }
 
-    pub fn hitSphere(ray: Ray, sphere_center: Vec3, radius: f32) bool {
+    pub fn hitSphere(ray: Ray, sphere_center: Vec3, radius: f32) f32 {
         const oc: Vec3 = ray.origin.sub(sphere_center);
         const a: f32 = ray.direction.dot(ray.direction);
         const b: f32 = oc.dot(ray.direction) * 2.0;
         const c: f32 = oc.dot(oc) - (radius * radius);
         const discriminant: f32 = (b * b) - (a * c * 4.0);
-        return discriminant > 0.0;
+
+        if (discriminant < 0) {
+            return -1.0;
+        } else {
+            return (-b - @sqrt(discriminant)) / (2.0 * a);
+        }
     }
 
     pub fn color(ray: Ray) Vec3 {
-        if (ray.hitSphere(Vec3.init(0.0, 0.0, -1.0), 0.5)) {
-            return Vec3.init(1.0, 0.0, 0.0);
+        var hit_point: f32 = ray.hitSphere(Vec3.init(0.0, 0.0, -1.0), 0.5);
+        if (hit_point > 0.0) {
+            const normal: Vec3 = ray.at(hit_point).sub(Vec3.init(0.0, 0.0, -1.0)).unit();
+            return Vec3.init(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0).multiplyBy(0.5);
         }
 
         const unit_direction: Vec3 = Vec3.unit(ray.direction);
-        const distance: f32 = 0.5 * (unit_direction.y + 1.0);
+        hit_point = 0.5 * (unit_direction.y + 1.0);
         const fromColor: Vec3 = Vec3.init(1.0, 1.0, 1.0);
         const toColor: Vec3 = Vec3.init(0.5, 0.7, 1.0);
 
-        return fromColor.multiplyBy(1.0 - distance).add(toColor.multiplyBy(distance));
+        return fromColor.multiplyBy(1.0 - hit_point).add(toColor.multiplyBy(hit_point));
     }
 };
 
