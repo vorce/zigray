b064877548dc144ff84d60c77dce04a1b7e0c8b2
Joel C <joel@vorce.se>
2021-11-29 16:54:46 +0100

    Red Sphere of doom

--- src/ray.zig
+++ src/ray.zig
@@ -17,7 +17,20 @@ pub const Ray = struct {
         return self.origin.add(self.direction.multiplyBy(distance));
     }
 
+    pub fn hitSphere(ray: Ray, sphere_center: Vec3, radius: f32) bool {
+        const oc: Vec3 = ray.origin.sub(sphere_center);
+        const a: f32 = ray.direction.dot(ray.direction);
+        const b: f32 = oc.dot(ray.direction) * 2.0;
+        const c: f32 = oc.dot(oc) - (radius * radius);
+        const discriminant: f32 = (b * b) - (a * c * 4.0);
+        return discriminant > 0.0;
+    }
+
     pub fn color(ray: Ray) Vec3 {
+        if (ray.hitSphere(Vec3.init(0.0, 0.0, -1.0), 0.5)) {
+            return Vec3.init(1.0, 0.0, 0.0);
+        }
+
         const unit_direction: Vec3 = Vec3.unit(ray.direction);
         const distance: f32 = 0.5 * (unit_direction.y + 1.0);
         const fromColor: Vec3 = Vec3.init(1.0, 1.0, 1.0);
